55c2c304284ee3333961466e8d64d12fbcd98f8e
Leah Stohr <leah.stohr@gmail.com>
2015-08-07 13:49:46 -0600

    a second file is added

---
+++ 09-functions-1.py
@@ -0,0 +1,52 @@
+'''
+
+Functions are a way of grouping code together. This allows the programmer to break code into easy to use chunks, that can be called over and over again. 
+
+Here are the key things to learn:
+
+1) A function can take in some data, called arguments or parameters.
+
+2) Functions can return values that can then be assigned to variables or evaluated.
+
+3) The "return" keyword will return a value from a function.
+
+Hint: Using the "pass" keyword, you can create empty functions that does nothing.
+
+'''
+
+#create a function called my_func that does nothing
+def my_func():
+	pass
+
+#create a function with one argument that does nothing
+
+
+#create a function with two arguments that does nothing
+
+
+#create a function that returns the string "my return value"
+
+assert "my return value" == func_return()
+
+#create a func that returns the integer 5
+
+assert 5 == func_return_int()
+
+
+#create a func that takes in two arguments and adds them up
+
+assert 9 == func_return_add(5,4)
+
+#create a function that returns True
+
+assert func_return_bool()
+
+#create a function that takes in a number and does a conversion from celsius to farenheit 
+
+
+
+
+
+
+
+
